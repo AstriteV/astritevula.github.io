fd5d33e8c02a395f568c866bcbabc4b5c2eb9fed
AstriteV <astritev@gmail.com>
2026-03-09 18:15:33 +0000

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,14 @@
-document.querySelectorAll(".acc-btn").forEach(btn=>{
-  btn.addEventListener("click",()=>{
-    const panel=document.getElementById(btn.dataset.target);
+document.querySelectorAll(".acc-btn").forEach(btn => {
+
+  const panel = document.getElementById(btn.dataset.target);
+
+  // open newest event automatically
+  if (btn.dataset.defaultOpen === "true") {
+    panel.classList.add("open");
+  }
+
+  btn.addEventListener("click", () => {
     panel.classList.toggle("open");
   });
+
 });
